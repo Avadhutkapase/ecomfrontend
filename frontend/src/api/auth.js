3d03544b3abf44ae0932db1c0ac3685f3c99ae6e
avadhutkapase <avadhutkapase@gmail.com>
2025-02-21 02:11:44 -0800

    final done

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,5 @@
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://ecom-backend-ko78.onrender.com";
 
 export async function login(credentials) {
   const response = await fetch(`${API_URL}/login`, {
